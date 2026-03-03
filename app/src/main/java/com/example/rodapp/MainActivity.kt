672a1e86fc2617a87c447184971954f918792674
Jose Santiago Martinez Martinez <jsantiagomartinez@ucompensar.edu.co>
2026-03-02 19:12:22 -0500

    cambio a linear layout 2-03-2026

--- app/src/main/java/com/example/rodapp/MainActivity.kt
+++ app/src/main/java/com/example/rodapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tv_bienvenida)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -19,4 +19,4 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-//NO TOCAR PERROS HP
+// NO TOCAR PERROS HP
