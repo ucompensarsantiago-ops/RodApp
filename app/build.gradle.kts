63cb00d5a16e86b9204e9108487842bcfe4cd44b
Jose Santiago Martinez <jsantiagomartinez@ucompensar.edu.co>
2026-04-06 21:25:42 -0500

    clase 6 abril registro users supabase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -47,4 +48,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.supabase.bom))
+    implementation(libs.supabase.postgrest)
+    implementation(libs.supabase.auth)
+    implementation(platform(libs.ktor.bom))
+    implementation(libs.ktor.client.android)
+    implementation(libs.kotlinx.serialization.json)
+
+
+
+
 }
---
+++ app/src/main/java/com/example/rodapp/SupabaseClient.kt
@@ -0,0 +1,15 @@
+package com.example.rodapp
+
+import io.github.jan_tennert.supabase.createSupabaseClient
+import io.github.jan_tennert.supabase.auth.Auth
+import io.github.jan_tennert.supabase.postgrest.Postgrest
+
+object SupabaseClient {
+    val client = createSupabaseClient(
+        supabaseUrl = "https://erttcudseqjrpyathmal.supabase.co",
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVydHRjdWRzZXFqcnB5YXRobWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTUwMzUsImV4cCI6MjA5MTA5MTAzNX0.zmyADFrL2iPvGMhy9eU2SQ7VWZsF_h2E2D86RpMzgWA"
+    ) {
+        install(Auth)
+        install(Postgrest)
+    }
+}
